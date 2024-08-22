1f2b716e5300fa562efc8e542ba96e32f4ddcd8f
yeeeeeeeop <yeop0916@gmail.com>
2024-08-22 13:37:53 +0900

    [Silver II] Title: 스택 수열, Time: 300 ms, Memory: 27584 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+
+    public void solve() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder answer = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        boolean cannot = false;
+
+        Stack<Integer> stack = new Stack<>();
+
+        int num = 1;
+        for (int i = 0; i < n; i++) {
+            int target = Integer.parseInt(br.readLine());
+            while (num <= target) {
+                answer.append("+\n");
+                stack.push(num++);
+            }
+
+            int top = stack.pop();
+            answer.append("-\n");
+
+            if (top > target) {
+                answer = new StringBuilder("NO");
+                break;
+            }
+        }
+
+        bw.write(answer + "");
+        bw.flush();
+        bw.close();
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Solution sol = new Solution();
+        sol.solve();
+    }
+}
